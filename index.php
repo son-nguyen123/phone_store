e93e3d78aa03af67e9ec35e4a47da3cc69b4d4f3
son-nguyen123 <sonnguyenminhdanang@gmail.com>
2024-11-19 15:41:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -171,8 +171,10 @@ echo "<script>console.log(" . json_encode($iphones) . ");</script>";
 	<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
 		<div class="container">
 			<a class="navbar-brand" href="#page-top"></a>
+			
 
 			<div class="collapse navbar-collapse" id="navbarResponsive">
+				
 				<ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
 					<li class="nav-item"><a class="nav-link" href="#services" style=" padding-bottom: 60%">Services</a></li>
 					<li class="nav-item"><a class="nav-link" href="#portfolio" style=" padding-bottom: 60%">Portfolio</a></li>
