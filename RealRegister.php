f11d67b51bcb9d7d6454074d5610e8b02a80414f
Jade2308 <vinhsanh2308@gmail.com>
2024-12-14 12:34:48 +0700

    .

--- RealRegister.php
+++ RealRegister.php
@@ -6,37 +6,28 @@ RealRegister
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Colo Shop Template">
-    <title>Colo Shop</title>
-    <link rel="icon" href="icon.png" type="image/png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="LoginStyle.css">
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/footer.css">
+    <title>SanhSon</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="plugins/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
-    <link rel="stylesheet" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
-    <link rel="stylesheet" href="plugins/OwlCarousel2-2.2.1/animate.css">
     <link rel="stylesheet" href="styles/main_styles.css">
     <link rel="stylesheet" href="styles/responsive.css">
+    <link rel="stylesheet" href="css/footer.css">
     <style>
         body {
-            display: flex;
-            flex-direction: column;
-            height: 100vh;
-            margin: 0;
             background-color: #f2f2f2;
-            font-family: Arial, sans-serif;
         }
         .main-content {
             display: flex;
             justify-content: center;
             align-items: center;
             flex-grow: 1;
+            font-family: Arial, sans-serif;
+            margin-top: 70px;
+            margin-bottom: 20px;
         }
         .register-container {
-            width: 400px;
+            width: 500px;
             padding: 40px;
             background-color: #ffffff;
             box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
@@ -57,9 +48,25 @@ RealRegister
         }
         .form-control {
             width: 100%;
+            color: #777777;
         }
         .btn-block {
             width: 100%;
+        }
+        .button {
+            width: 100%;
+            padding: 12px;
+            border: none;
+            border-radius: 4px;
+            background-color: #3a3a3a;
+            color: #fff;
+            font-weight: bold;
+            cursor: pointer;
+            font-size: 1em;
+            margin-top: 20px;
+        }
+        .button:hover {
+            background-color: #212529;
         }
     </style>
 </head>
@@ -122,7 +129,7 @@ RealRegister
                     <label for="signup-dob">Date of Birth</label>
                     <input type="date" class="form-control" id="signup-dob" name="dob" required>
                 </div>
-                <button type="submit" name="signup" class="btn btn-primary btn-block">Sign Up</button>
+                <button class="button">Sign Up</button>
             </form>
         </div>
     </div>
